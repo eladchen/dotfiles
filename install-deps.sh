49460eed435429fd4c39673c4b9bd64eeea9287b
Elad Chen <3628913+eladchen@users.noreply.github.com>
2020-05-09 16:55:10 +0300

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -1,18 +1,21 @@
 # Homebrew
 # https://brew.sh
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# git-friendly
-# https://github.com/jamiew/git-friendly
-curl -sS https://raw.githubusercontent.com/jamiew/git-friendly/master/install.sh | bash -s $(brew --prefix)/bin
+if command -v brew > /dev/null 2>&1; then
+  brew update
+else
+	# This assumes ruby is available :)
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 # Node Version Manager (NVM)
 # https://github.com/nvm-sh/nvm
+# Installing / Upgrading is the same
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 
 # SDKMAN
 # https://sdkman.io/
-curl -s "https://get.sdkman.io" | bash
-
-# pip3 install pygments --upgrade
-# sudo -H python -m ensurepip
+if command -v sdk > /dev/null 2>&1; then
+  sdk selfupdate
+else
+  curl -s "https://get.sdkman.io" | bash
+fi
